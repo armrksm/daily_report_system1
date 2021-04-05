6d623d9a5ff1c980eab2bfcb16bb75cc3dcb9517
mona <asbmn919@gmail.com>
2021-04-06 02:23:26 +0900

    Add Employee index

--- src/controllers/client/ClientIndexServlet.java
+++ src/controllers/client/ClientIndexServlet.java
@@ -1,23 +1,18 @@
-package controllers.login;
+package controllers.client;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginServlet
- */
-@WebServlet("/LoginServlet")
-public class LoginServlet extends HttpServlet {
+@WebServlet("/index/client")
+public class ClientIndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
+
+    public ClientIndexServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
--- src/controllers/employees/EmployeesIndexServlet.java
+++ src/controllers/employees/EmployeesIndexServlet.java
@@ -14,23 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 import models.Employee;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class EmployeesIndexServlet
- */
 @WebServlet("/employees/index")
 public class EmployeesIndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public EmployeesIndexServlet() {
         super();
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
